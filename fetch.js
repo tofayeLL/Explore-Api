9309aff256c09158786b8f524f90bc9997d211b4
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-26 23:02:02 +0600

    learn  about fetch

---
+++ fetch.js
@@ -0,0 +1,19 @@
+/* fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json()) 
+      // .json is not similer but close JSON.parse
+      .then(json => console.log(json))
+ */
+
+
+     /*  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+      fetch (url)
+      .then (response => response.json())
+      .then (json => console.log(json)) */
+
+
+    function loadData (){
+        const url = 'https://jsonplaceholder.typicode.com/todos/1';
+        fetch (url)
+        .then (response => response.json())
+        .then (json => console.log(json))
+    }
